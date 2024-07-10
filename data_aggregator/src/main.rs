305fa2ec814df26c1c4d7906c562691e9909b2bf
Mikołaj Woźniak <mw@shardlabs.io>
2024-07-10 15:41:58 +0200

    Add fetching real transaction data from helius api

--- data_aggregator/src/main.rs
+++ data_aggregator/src/main.rs
@@ -1,36 +1,28 @@
-use solana_sdk::{pubkey::Pubkey, signature::Signature};
-use solana_client::{rpc_client::{GetConfirmedSignaturesForAddress2Config, RpcClient}, rpc_config::RpcTransactionConfig};
-use solana_transaction_status::UiTransactionEncoding;
+use helius::error::Result;
+use helius::types::*;
+use helius::Helius;
+use solana_client::rpc_client::GetConfirmedSignaturesForAddress2Config;
+use solana_sdk::pubkey::Pubkey;
 
-fn main() {
-    // URL to Solana RPC
-    let rpc_url = "https://api.mainnet-beta.solana.com";
-    let client = RpcClient::new(rpc_url.to_string());
-
-    // Replace the following public key with your account's public key
-    let account_pubkey_str = "4tBxVjsG4pUTnust1MKYzSj7cLcdVP3HPpR7cCdxFX6d";
-    let account_pubkey = account_pubkey_str.parse::<Pubkey>().unwrap();
-
-    // Fetch account data
-    match client.get_account(&account_pubkey) {
-        Ok(account) => {
-            // Display account data
-            println!("Account data: {:?}", account);
-
-            // Convert lamports to SOL (1 SOL = 1_000_000_000 lamports)
-            let sol_balance = account.lamports as f64 / 1_000_000_000.0;
-            println!("Balance: {} SOL", sol_balance);
-        },
-        Err(err) => {
-            // Display error message if fetching account data fails
-            eprintln!("Error fetching account data: {:?}", err);
-        }
-    }
-
+#[tokio::main]
+async fn main() -> Result<()> {
     // TODO: add mapping from encoded tx
     // TODO: add crawler in time logic
+    // TODO: handle unwraps
+    // TODO: handle cli arg
 
-    // Define the configuration for fetching transaction signatures
+    let api_key: &str = "24cf0798-4008-4c81-aa5e-2875323278cd";
+    let cluster: Cluster = Cluster::MainnetBeta;
+
+    let helius: Helius = Helius::new(api_key, cluster).unwrap();
+
+    // Replace the following public key with your account's public key
+    let account_pubkey_str = "4LLVkfZMp5hh1fEKVexyMNSLjpJyQswvXMW76DaTpyqJ";
+    let account_pubkey = account_pubkey_str.parse::<Pubkey>().unwrap();
+
+    let account = helius.rpc().solana_client.get_account(&account_pubkey);
+    println!("account: {:?}", account);
+
     let signature_config = GetConfirmedSignaturesForAddress2Config {
         before: None,
         until: None,
@@ -38,48 +30,42 @@ fn main() {
         commitment: None,
     };
 
-    // Fetch transaction signatures
-    match client.get_signatures_for_address_with_config(&account_pubkey, signature_config) {
-        Ok(signatures) => {
-            println!("Fetch transaction signatures");
+    let signatures = helius
+        .rpc()
+        .solana_client
+        .get_signatures_for_address_with_config(&account_pubkey, signature_config);
 
-            for signature_info in signatures {
-                println!("signature_info block_time: {:?}", signature_info.block_time);
-                println!("signature_info slot: {:?}", signature_info.slot);
-                println!("signature_info signature: {:?}", signature_info.signature);
+    for signature_info in signatures.unwrap() {
+        println!("signature_info block_time: {:?}", signature_info.block_time);
+        println!("signature_info slot: {:?}", signature_info.slot);
+        println!("signature_info signature: {:?}", signature_info.signature);
+    
+        let request: ParseTransactionsRequest = ParseTransactionsRequest {
+            transactions: vec![
+                signature_info.signature,
+            ],
+        };
+    
+        let response = helius.parse_transactions(request).await;
+        let txs = response.unwrap();
+        for tx in txs {
+            println!("signature: {:?}", tx.signature);
 
-                // Convert String to Signature
-                let signature = signature_info.signature.parse::<Signature>().unwrap();
-
-                // Fetch transaction details for each signature
-                match client.get_transaction_with_config(&signature, RpcTransactionConfig {
-                    encoding: Some(UiTransactionEncoding::Json),
-                    commitment: None,
-                    max_supported_transaction_version: Some(0),
-                }) {
-                    Ok(transaction) => {
-                        println!("Transaction block_time: {:?}", transaction.block_time);
-                        println!("Transaction transaction: {:?}", transaction.transaction);
-                        // println!("Transaction transaction.transaction: {:?}", transaction.transaction.transaction);
-                    },
-                    Err(err) => {
-                        eprintln!("Error fetching transaction details for signature {}: {:?}", signature_info.signature, err);
-                    }
-                }
-                
-                // // Fetch transaction details for each signature
-                // match client.get_transaction(&signature_info.signature, RpcTransactionConfig { encoding: None, commitment: None }) {
-                //     Ok(transaction) => {
-                //         println!("Transaction details: {:?}", transaction);
-                //     },
-                //     Err(err) => {
-                //         eprintln!("Error fetching transaction details for signature {}: {:?}", signature_info.signature, err);
-                //     }
-                // }
+            println!("description: {:?}", tx.description);
+            println!("transaction_type: {:?}", tx.transaction_type);
+            println!("transaction_error: {:?}", tx.transaction_error);
+            println!("fee: {:?}", tx.fee);
+            println!("source: {:?}", tx.source);
+            println!("slot: {:?}", tx.slot);
+            println!("timestamp: {:?}", tx.timestamp);
+ 
+            for native_transfer in tx.native_transfers.unwrap() {
+                println!("from_user_account: {:?}", native_transfer.user_accounts.from_user_account);
+                println!("to_user_account: {:?}", native_transfer.user_accounts.to_user_account);
+                println!("amount: {:?}", native_transfer.amount);
             }
-        },
-        Err(err) => {
-            eprintln!("Error fetching transaction signatures: {:?}", err);
         }
     }
+
+    Ok(())
 }
